39b4d11a29a44103038e0510650ff8d98b86dda3
wsaimee <66040295@kmitl.ac.th>
2026-02-12 18:43:17 +0700

    Delete cell03/ex02 directory

--- cell03/ex02/i_got_that.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-while True:
-    user_input = input("What you gotta say? ")
-
-    if user_input == "STOP":
-        break
-    else:
-        print("I got that! Anything else?")
