9b4edbaefe8a5b01b8a4853d0b2cc1355e94a9ee
wsaimee <66040295@kmitl.ac.th>
2026-02-12 18:24:10 +0700

    Create advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+i = 0
+while i <= 10:
+    j = 0
+    line = f"Table de {i}:"
+    while j <= 10:
+        line += f" {i * j}"
+        j += 1
+    print(line)
+    i += 1
